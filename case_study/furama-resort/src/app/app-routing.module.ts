5bb9e01857e9f804d77fb2094ab36c7317141728
unknown <letranthaovy1326@gmail.com>
2022-02-14 00:23:46 +0700

    up fix up

--- case_study/furama-resort/src/app/app-routing.module.ts
+++ case_study/furama-resort/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerListComponent} from './customer/customer-list/customer-list.component';
+import { ServiceListComponent} from './service/service-list/service-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CustomerListComponent},
+  {path: 'serviceList', component: ServiceListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
